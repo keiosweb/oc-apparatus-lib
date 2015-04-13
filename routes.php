7596065d57e87b605255b3cb7e03d876644fe25e
Łukasz Biały <lukasz@c-call.eu>
2015-04-13 13:27:47 +0200

    added backend admin access route to protected files

---
+++ routes.php
@@ -0,0 +1,25 @@
+<?php
+
+Route::get(
+    '/storage/app/uploads/protected/{slug}',
+    function ($slug) {
+        $path = storage_path().'/app/uploads/protected/'.$slug;
+
+        if (!\Backend\Facades\BackendAuth::check()) {
+            return Redirect::to('/404');
+        }
+
+        if (!File::exists($path)) {
+            return Redirect::to('/404');
+        }
+
+        $segmentizedUrl = \October\Rain\Router\Helper::segmentizeUrl(Request::url());
+        $diskName = array_pop($segmentizedUrl);
+
+        if (!$file = \System\Models\File::where('disk_name', $diskName)->first()) {
+            return Redirect::to('/404');
+        };
+
+        return $file->output('attachment');
+    }
+)->where('slug', '(.*)?');
