0101ee7127d92700ced28aa09f74fd6b749eb454
Kuba <jakub@c-call.eu>
2015-09-13 22:45:34 +0200

    polish translations

---
+++ lang/pl/lang.php
@@ -0,0 +1,27 @@
+<?php
+
+return [
+    'errors'      => [
+        'pageWithComponentNotFound' => 'Komponent %s nie jest przydzielony do żadnej strony CMS.',
+        'parameterNotFound'         => 'Parametr %s nie został znaleziony w konfiguracji komponentu %s.',
+    ],
+    'settings'    => [
+        'messaging-label'          => 'Wiadomości',
+        'messaging-description'    => 'Udostępnia silnik powiadomień',
+        'messaging-tab'            => 'Wiadomości',
+        'messaging-layout'         => 'Layout',
+        'messaging-openAnimation'  => 'Animacja otwierania',
+        'messaging-closeAnimation' => 'Animacja zamykania',
+        'messaging-theme'          => 'Motyw',
+        'messaging-template'       => 'Szablon',
+        'messaging-timeout'        => 'Timeout',
+        'messaging-dismissQueue'   => 'Odrzucanie kolejki',
+        'messaging-force'          => 'Wymuszanie',
+        'messaging-modal'          => 'Pokaż jako modal',
+        'messaging-maxVisible'     => 'Maksymalny czas widoczności',
+    ],
+    'permissions' => [
+        'tab'             => 'Apparatus',
+        'access_settings' => 'Dostęp do ustawień',
+    ],
+];
